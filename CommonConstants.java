4a7220c7543cd1493e8bfc13ce418653fc477b84
minhthong1410 <126005323+minhthong1410@users.noreply.github.com>
2023-02-22 00:30:06 +0700

    Add files via upload

---
+++ CommonConstants.java
@@ -0,0 +1,24 @@
+package constants;
+
+public class CommonConstants {
+    // cònigs
+    public static final String APP_NAME = "Calculator";
+    public static final int[] APP_SIZE = {400, 600}; // {rộng, cao}
+
+    //TEXTFIELDS CONFIGS
+    public  static  final int TEXTFIELD_LENGTH = 10;
+    public  static  final int TEXTFIELD_FONTSIZE = 36;
+    public  static final int TEXTFIELD_SPRINGLAYOUT_NORTHPAD = 30;
+    public  static final int TEXTFIELD_SPRINGLAYOUT_WESTPAD = 25;
+
+    //BUTTON CONFIGS
+    public static final int BUTTON_ROWCOUNT = 4;
+    public static final int BUTTON_COLCOUNT = 4;
+    public  static final int BUTTON_COUNT = 16;
+    public static final int BUTTON_FONTSIZE = 46;
+    public  static final int BUTTON_SPRINGLAYOUT_NORTHPAD = 150;
+    public  static final int BUTTON_SPRINGLAYOUT_WESTPAD = 33;
+    public static final int BUTTON_HGAP = 7;
+    public static final int BUTTON_VGAP = 7;
+
+}
